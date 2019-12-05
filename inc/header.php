164ae02b7aa891fb5e6bf31282e196cf578d25b1
CoDemented <52868823+CoDemented@users.noreply.github.com>
2019-12-05 22:41:05 +0500

    added some files

---
+++ inc/header.php
@@ -0,0 +1,16 @@
+<?php
+    define("_SITE_URL", "http://localhost/cm-php/");
+    define("_ASSETS", _SITE_URL . "assets/");
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="<?php echo _ASSETS; ?>css/fa-pro/css/all.css">
+    <link rel="stylesheet" href="<?php echo _ASSETS; ?>css/normalize.css">
+    
+    <title>BP-Project</title>
+</head>
+<body>
